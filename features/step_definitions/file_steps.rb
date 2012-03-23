e89be1911a82ece64d468561da919ff6aa1f11fb
Arnab <arnab.deka@gmail.com>
2012-03-22 21:01:46 -0700

    Moved example files from inside features/support to root, since they are useful even outside of tests

--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -2,7 +2,7 @@ When /^I upload the file "([^"]*)"$/ do |filename|
   steps %Q{
       When I go to the file import page
     }
-  path = File.join(::Rails.root, 'features', 'support', 'examples', filename)
+  path = File.join(::Rails.root, 'examples', filename)
   attach_file("file", path)
   click_button("Upload")
 end
