946010ff3343ab4c1f4caf612e96690d97644d70
Arnab <arnab.deka@gmail.com>
2012-03-27 19:29:38 -0700

    Purchase specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,20 @@
 FactoryGirl.define do
   factory :user do
-    name     "Foo"
-    email    { "#{name}@bar.com" }
-    password "bazbazbaz+%"
+    name                  "Foo"
+    email                 { "#{name}@bar.com" }
+    password              "bazbazbaz+%"
     password_confirmation { password }
+  end
+
+  factory :purchase do
+    purchaser_name "Foo"
+    quantity       1
+  end
+
+  factory :deal do
+    description      "$1mm awesomeness for $10k"
+    price            10_000
+    merchant_name    "me me me"
+    merchant_address "my place"
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Purchase do
+  before { @purchase = FactoryGirl.build(:purchase) }
+
+  subject { @purchase }
+
+  it { should respond_to(:deal) }
+
+  describe "when not attached to a deal" do
+    it { should_not be_valid }
+  end
+
+  describe "when attached to a deal" do
+    before do
+      deal = FactoryGirl.create(:deal)
+      @purchase.deal = deal
+    end
+
+    it { should be_valid }
+  end
 end
